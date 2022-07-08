90fd34c2e9e4f8b70ec47cffc64f45309168cbd1
thiagoaacruz <thiagoaacruz@gmail.com>
2022-07-08 13:44:29 -0300

    Criando no controller Serviço para deletar Curso específico

--- src/main/java/br/thiago/projetofinal/controllers/CursoController.java
+++ src/main/java/br/thiago/projetofinal/controllers/CursoController.java
@@ -1,14 +1,18 @@
 package br.thiago.projetofinal.controllers;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,18 +23,19 @@ import br.thiago.projetofinal.repository.ICurso;
 import br.thiago.projetofinal.request.CursoGetResponse;
 import br.thiago.projetofinal.request.CursoPostRequest;
 import br.thiago.projetofinal.request.CursoPutRequest;
+import io.swagger.annotations.ApiOperation;
 
 @Controller
 @CrossOrigin(origins = "", allowedHeaders = "")
 public class CursoController {
-	
+
 	@Autowired
 	ICurso repository;
-	
+
 	private static final String ENDPOINT = "api/curso";
-	
-	
-	//@ApiOperation("Serviço para cadastro de Curso")
+
+	@ApiOperation("Serviço para cadastro de Curso")
+
 	@RequestMapping(value = ENDPOINT, method = RequestMethod.POST)
 	public ResponseEntity<String> post(@RequestBody CursoPostRequest request) {
 		try {
@@ -41,8 +46,6 @@ public class CursoController {
 			curso.setDataTermino(request.getDataTermino());
 			curso.setQuantidadeAluno(request.getQuantidadeAluno());
 			curso.setCategoria(request.getCategoria());
-			
-			
 
 			repository.save(curso);
 
@@ -51,13 +54,13 @@ public class CursoController {
 		} catch (Exception e) {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro" + e.getMessage());
 		}
-	
+
 	}
-	
+
 //_______________________________________________________________________________________________________________________________	
-	
-	
-	//@ApiOperation("Serviço para atualização dos dados de um Curso")
+
+	@ApiOperation("Serviço para atualização dos dados de um Curso")
+
 	@RequestMapping(value = ENDPOINT, method = RequestMethod.PUT)
 	public ResponseEntity<String> put(@RequestBody CursoPutRequest request) {
 		try {
@@ -69,13 +72,13 @@ public class CursoController {
 			} else {
 
 				Curso curso = item.get();
-				
+
 				curso.setDescricaoAssunto(request.getDescricaoAssunto());
 				curso.setDataInicio(request.getDataInicio());
 				curso.setDataTermino(request.getDataTermino());
 				curso.setQuantidadeAluno(request.getQuantidadeAluno());
 				curso.setCategoria(request.getCategoria());
-				
+
 				repository.save(curso);
 
 				return ResponseEntity.status(HttpStatus.OK).body("Atualizado");
@@ -86,11 +89,11 @@ public class CursoController {
 		}
 
 	}
-	
+
 //_______________________________________________________________________________________________________________________________		
 
-	
-	//@ApiOperation("Serviço para deletar Curso")
+	@ApiOperation("Serviço para deletar Curso")
+
 	@RequestMapping(value = ENDPOINT + "/{idcurso}", method = RequestMethod.DELETE)
 	public ResponseEntity<String> delete(@PathVariable("idcurso") Integer idcurso) {
 		try {
@@ -109,12 +112,11 @@ public class CursoController {
 		}
 
 	}
-	
-	
-//_______________________________________________________________________________________________________________________________			
-	
 
-	//@ApiOperation("Serviço para consultar todos os Cursos")
+//_______________________________________________________________________________________________________________________________			
+
+	@ApiOperation("Serviço para consultar todos os Cursos")
+
 	@RequestMapping(value = ENDPOINT, method = RequestMethod.GET)
 	public ResponseEntity<List<CursoGetResponse>> get() {
 
@@ -127,21 +129,19 @@ public class CursoController {
 			item.setIdcurso(curso.getIdcurso());
 			item.setDescricaoAssunto(curso.getDescricaoAssunto());
 			item.setDataInicio(curso.getDataInicio());
-			item.setDataTermino(curso.getDataTermino());			
-			item.setQuantidadeAluno(curso.getQuantidadeAluno());			
+			item.setDataTermino(curso.getDataTermino());
+			item.setQuantidadeAluno(curso.getQuantidadeAluno());
 			item.setCategoria(curso.getCategoria());
-			
 
 			response.add(item);
 		}
 		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
-	
+
 //_______________________________________________________________________________________________________________________________			
-	
-	
-	
-	//@ApiOperation("Serviço para consultar todos os Cursos por id")
+
+	@ApiOperation("Serviço para consultar todos os Cursos por id")
+
 	@RequestMapping(value = ENDPOINT + "/{idcurso}", method = RequestMethod.GET)
 	public ResponseEntity<CursoGetResponse> getId(@PathVariable("idcurso") Integer idcurso) {
 		Optional<Curso> item = repository.findById(idcurso);
@@ -159,13 +159,96 @@ public class CursoController {
 			response.setDataTermino(curso.getDataTermino());
 			response.setQuantidadeAluno(curso.getQuantidadeAluno());
 			response.setCategoria(curso.getCategoria());
-					
-				
 
 			return ResponseEntity.status(HttpStatus.OK).body(response);
 		}
 
 	}
-	
+
+//_______________________________________________________________________________________________________________________________			
+
+	@ApiOperation("Serviço para consultar pelo nome")
+
+	@GetMapping(value = "/descricaoAssunto")
+	public ResponseEntity<List<CursoGetResponse>> listDescricaoAssunto(String descricaoAssunto) {
+
+		List<CursoGetResponse> response = new ArrayList<>();
+
+		for (Curso curso : repository.findByDescricaoAssunto(descricaoAssunto)) {
+
+			CursoGetResponse buscaPorNome = new CursoGetResponse();
+
+			buscaPorNome.setIdcurso(curso.getIdcurso());
+			buscaPorNome.setDescricaoAssunto(curso.getDescricaoAssunto());
+			buscaPorNome.setDataInicio(curso.getDataInicio());
+			buscaPorNome.setDataTermino(curso.getDataTermino());
+			buscaPorNome.setQuantidadeAluno(curso.getQuantidadeAluno());
+			buscaPorNome.setCategoria(curso.getCategoria());
+
+			response.add(buscaPorNome);
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(response);
+	}
+
+//_______________________________________________________________________________________________________________________________				
+
+	@ApiOperation("Serviço para consultar por data")
+
+	@GetMapping(value = "/faixa/{dataInicio}/{dataTermino}")
+	public ResponseEntity<List<CursoGetResponse>> listafaixa(
+			@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dataInicio,
+			@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dataTermino) {
+
+		List<CursoGetResponse> response = new ArrayList<>();
+
+		for (Curso curso : repository.findByDataInicioBetween(dataInicio, dataTermino)) {
+
+			CursoGetResponse buscaPorData = new CursoGetResponse();
+
+			buscaPorData.setIdcurso(curso.getIdcurso());
+			buscaPorData.setDescricaoAssunto(curso.getDescricaoAssunto());
+			buscaPorData.setDataInicio(curso.getDataInicio());
+			buscaPorData.setDataTermino(curso.getDataTermino());
+			buscaPorData.setQuantidadeAluno(curso.getQuantidadeAluno());
+			buscaPorData.setCategoria(curso.getCategoria());
+
+			response.add(buscaPorData);
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(response);
+	}
+
+//_______________________________________________________________________________________________________________________________					
+
+	@ApiOperation("Serviço para deletar Curso específico")
+
+	@DeleteMapping("/{idcurso}")
+	public ResponseEntity<String> apagar(@PathVariable("idcurso") Integer idcurso) {
+		try {
+			Optional<Curso> lista = repository.findById(idcurso);
+
+			if (lista.isEmpty()) {
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Curso não encontrado");
+
+			} else {
+
+				Curso curso = lista.get();
+
+				if (curso.getDataTermino().isBefore(LocalDate.now())) {//Adicionei esse lógica para verificar se a data final está antes
+
+					return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Curso não pode ser deletado");
+
+				} else {
+
+					repository.delete(curso);
+				}
+
+				return ResponseEntity.status(HttpStatus.OK).body("Curso excluido");
+			}
+			
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro :" + e.getMessage());
+		}
+
+	}
 
 }
--- src/main/java/br/thiago/projetofinal/repository/ICurso.java
+++ src/main/java/br/thiago/projetofinal/repository/ICurso.java
@@ -1,5 +1,8 @@
 package br.thiago.projetofinal.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +11,18 @@ import br.thiago.projetofinal.entity.Curso;
 @Repository
 public interface ICurso extends JpaRepository<Curso, Integer> {
 	
-
-
+	
+	List<Curso> findByDescricaoAssunto(String descricaoAssunto);
+	
+	
+	/*<p>Este método atua como uma ponte entre o baseado em array e o baseado em coleção
+    * API.
+    *
+    * @return um array contendo todos os elementos desta lista no devido*/
+	
+	List<Curso> findByDataInicioBetween(LocalDate dataInicio, LocalDate dataTermino);
+	
+	
+	
+	
 }
